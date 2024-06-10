3702918154644319323331dc84378faf60ead08e
felliperfm <felliperfm@gmail.com>
2024-06-10 20:39:42 -0300

    created functions

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+    let strLower = str.toLowerCase();
+    let strLowerClean = strLower.replaceAll(" ", "");
+    let strLowerCleanReversed = "";
+    for (let i = strLowerClean.length - 1; i >= 0; i--) {
+        strLowerCleanReversed += strLowerClean[i]
+    }
+    return strLowerCleanReversed == strLowerClean ? true : false;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+    let max = arr[0];
+    let min = arr[0];
+    let arrMaxMin;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+    }
+    arrMaxMin = [min, max]
+    return arrMaxMin
 }
